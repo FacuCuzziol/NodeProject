0cd3a3d2b0c811a53128004e99872b7f3a0b3cf3
FacuCuzziol <facucuzziol@gmail.com>
2020-04-17 21:32:11 -0300

    getting things out from database to display on website

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+
+const Viaje = require('../models/Viajes');
+
 module.exports = function(){
     
     router.get('/',(req,res) =>{
@@ -11,6 +14,15 @@ module.exports = function(){
         res.render('nosotros',{
             pagina : 'Sobre Nosotros'
         });
+    });
+
+    router.get('/viajes',(req,res) =>{
+        Viaje.findAll()
+            .then(viajes => res.render('viajes',{
+                pagina : 'Proximos Viajes',
+                viajes
+            }))
+            .catch(error => console.log(error))
     });
 
     return router;
